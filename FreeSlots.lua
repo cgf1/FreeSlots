ef001b2417bd5d8a5bf60acddddf2d0aa6afbc57
Chris Faylor <cgf@realedsolutions.com>
2021-01-24 13:30:27 -0500

    reformat

--- FreeSlots.lua
+++ FreeSlots.lua
@@ -3,40 +3,39 @@
 -- Description: Displays free inventory slots without opening the bag screen.
 
 local wm = GetWindowManager()
-local em = GetEventManager()
-local print = d
+local settings
 
 FreeSlots = {}
 FreeSlots.name = "FreeSlots"
-FreeSlots.version	= "2.1.8"
-FreeSlots.settingsVersion = 1
+FreeSlots.version = "2.1.8"
+settingsVersion = 1
 
 local settingsDefaults = {
-	wm = {
-		x = 30,
-		y = 1035,
-		width = 180,
-		height = 100
-	}
+    wm = {
+        x = 30,
+        y = 1035,
+        width = 180,
+        height = 100
+    }
 }
 
 local function initialize(eventCode, addOnName)
-	if (addOnName ~= FreeSlots.name) then
-		return
-	end
-	EVENT_MANAGER:UnregisterForEvent("FreeSlots")
+    if (addOnName ~= FreeSlots.name) then
+        return
+    end
+    EVENT_MANAGER:UnregisterForEvent("FreeSlots")
 
-	FreeSlots.settings = ZO_SavedVars:New("FreeSlotsSettings" , FreeSlots.settingsVersion, nil, settingsDefaults, nil)
+    settings = ZO_SavedVars:New("FreeSlotsSettings" , settingsVersion, nil, settingsDefaults, nil)
 
-	FreeSlotsUI:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, FreeSlots.settings.wm.x, FreeSlots.settings.wm.y)
-	FreeSlotsUI:SetWidth(FreeSlots.settings.wm.width)
-	FreeSlotsUI:SetHeight(FreeSlots.settings.wm.height)
+    FreeSlotsUI:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, settings.wm.x, settings.wm.y)
+    FreeSlotsUI:SetWidth(settings.wm.width)
+    FreeSlotsUI:SetHeight(settings.wm.height)
 
-	local fragment = ZO_SimpleSceneFragment:New(FreeSlotsUI)
-	local sceneHud = SCENE_MANAGER:GetScene("hud")
-	local sceneHudUI = SCENE_MANAGER:GetScene("hudui")
-	sceneHud:AddFragment(fragment)
-	sceneHudUI:AddFragment(fragment)
+    local fragment = ZO_SimpleSceneFragment:New(FreeSlotsUI)
+    local sceneHud = SCENE_MANAGER:GetScene("hud")
+    local sceneHudUI = SCENE_MANAGER:GetScene("hudui")
+    sceneHud:AddFragment(fragment)
+    sceneHudUI:AddFragment(fragment)
 end
 
 EVENT_MANAGER:RegisterForEvent("FreeSlots" , EVENT_ADD_ON_LOADED , initialize)
@@ -77,7 +76,6 @@ end
 
 
 local function ShowFreeSlots()
-
     red = 0
     green = 0
     blue = 0
@@ -86,89 +84,89 @@ local function ShowFreeSlots()
 
     --red
     NoSpace = {}
-for i=-1, 5 do
-  NoSpace[#NoSpace+1] = i + 1
-end
+    for i=-1, 5 do
+        NoSpace[#NoSpace+1] = i + 1
+    end
 
     for k = 1, #NoSpace do
-	    if CheckInventorySpaceSilently(NoSpace[k]) == true then
-		    red = 255
-		    green = 0
-		    blue = 0
-		    alpha = 255
-	    end
+        if CheckInventorySpaceSilently(NoSpace[k]) == true then
+            red = 255
+            green = 0
+            blue = 0
+            alpha = 255
+        end
     end
 
     --orange
     SomeSpace = {}
-for i=6, 10 do
-  SomeSpace[#SomeSpace+1] = i + 1
-end
+    for i=6, 10 do
+        SomeSpace[#SomeSpace+1] = i + 1
+    end
 
     for k = 1, #SomeSpace do
-	    if CheckInventorySpaceSilently(SomeSpace[k]) == true then
-		    red = 128
-		    green = 0
-		    blue = 0
-		    alpha = 255
-	    end
+        if CheckInventorySpaceSilently(SomeSpace[k]) == true then
+            red = 128
+            green = 0
+            blue = 0
+            alpha = 255
+        end
     end
 
     --yellow
     DecentSpace = {}
-for i=11, 20 do
-  DecentSpace[#DecentSpace+1] = i + 1
-end
+    for i=11, 20 do
+        DecentSpace[#DecentSpace+1] = i + 1
+    end
 
     for k = 1, #DecentSpace do
-	    if CheckInventorySpaceSilently(DecentSpace[k]) == true then
-		    red = 255
-		    green = 255
-		    blue = 0
-		    alpha = 255
-	    end
+        if CheckInventorySpaceSilently(DecentSpace[k]) == true then
+            red = 255
+            green = 255
+            blue = 0
+            alpha = 255
+        end
     end
 
     --blue
     MediumSpace = {}
-for i=21, 30 do
-  MediumSpace[#MediumSpace+1] = i + 1
-end
+    for i=21, 30 do
+        MediumSpace[#MediumSpace+1] = i + 1
+    end
 
     for k = 1, #MediumSpace do
-	    if CheckInventorySpaceSilently(MediumSpace[k]) == true then
-		    red = 0
-		    green = 255
-		    blue = 255
-		    alpha = 255
-	    end
+        if CheckInventorySpaceSilently(MediumSpace[k]) == true then
+            red = 0
+            green = 255
+            blue = 255
+            alpha = 255
+        end
     end
 
     --green
     PlentyOfSpace = {}
-for i=31, 1000 do
-  PlentyOfSpace[#PlentyOfSpace+1] = i + 1
-end
+    for i=31, 1000 do
+        PlentyOfSpace[#PlentyOfSpace+1] = i + 1
+    end
 
     for k = 1, #PlentyOfSpace do
-	    if CheckInventorySpaceSilently(PlentyOfSpace[k]) == true then
-		    red = 0
-		    green = 255
-		    blue = 0
-		    alpha = 255
-	    end
+        if CheckInventorySpaceSilently(PlentyOfSpace[k]) == true then
+            red = 0
+            green = 255
+            blue = 0
+            alpha = 255
+        end
     end
 
     --getBagSpace
     slots = {}
-for i=0, 1000 do
-  slots[i] = i + 1
-end
+    for i=0, 1000 do
+        slots[i] = i + 1
+    end
 
     for j = 0, #slots do
-	    if CheckInventorySpaceSilently(j) == true then
-		    bagSpace = j
-	    end
+        if CheckInventorySpaceSilently(j) == true then
+            bagSpace = j
+        end
     end
 
     totalSlots = GetBagSize(BAG_BACKPACK)
@@ -189,10 +187,10 @@ end
 end
 
 function FreeSlots.OnMoveStop(self)
-    FreeSlots.settings.wm.x = self:GetLeft()
-    FreeSlots.settings.wm.y = self:GetTop()
-    FreeSlots.settings.wm.width = self:GetWidth()
-    FreeSlots.settings.wm.height = self:GetHeight()
+    settings.wm.x = self:GetLeft()
+    settings.wm.y = self:GetTop()
+    settings.wm.width = self:GetWidth()
+    settings.wm.height = self:GetHeight()
 end
 
 local BufferTable = {}
@@ -211,7 +209,7 @@ end
 
 function FreeSlots.OnUpdateHandler()
     if not BufferReached("myaddonupdatebuffer", 1) then
-	return
+        return
     end
     ShowFreeSlots()
 end
